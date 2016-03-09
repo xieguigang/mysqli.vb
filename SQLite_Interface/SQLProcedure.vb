4d5717a0745282a3645f4eed5a6ad87481634faf
xieguigang <xie.guigang@gmail.com>
2016-03-09 21:26:18 +0800

    sqlite references

--- SQLite_Interface/SQLProcedure.vb
+++ SQLite_Interface/SQLProcedure.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Data.Common
 Imports System.Text
 Imports System.Data.Linq.Mapping
+Imports System.Data.Entity.Core
 
 ''' <summary>
 ''' The API interface wrapper of the SQLite.(SQLite的存储引擎的接口)
@@ -360,17 +361,17 @@ Public Class SQLProcedure : Implements System.IDisposable
 
 End Class
 
-<Table(name:="table_schema")>
+<Table(Name:="table_schema")>
 Public Class TableDump
-    <Column(name:="guid", dbtype:="int", isprimarykey:=True)> Public Property Guid As Integer
-    <Column(name:="table_name", dbtype:="varchar(2048)")> Public Property TableName As String
-    <Column(name:="field", dbtype:="varchar(2048)")> Public Property FieldName As String
-    <Column(name:="dbtype", dbtype:="varchar(512)")> Public Property DbType As String
+    <Column(Name:="guid", DbType:="int", IsPrimaryKey:=True)> Public Property Guid As Integer
+    <Column(Name:="table_name", DbType:="varchar(2048)")> Public Property TableName As String
+    <Column(Name:="field", DbType:="varchar(2048)")> Public Property FieldName As String
+    <Column(Name:="dbtype", DbType:="varchar(512)")> Public Property DbType As String
     ''' <summary>
     ''' 1或者0
     ''' </summary>
     ''' <value></value>
     ''' <returns></returns>
     ''' <remarks></remarks>
-    <Column(name:="is_primary_key", dbtype:="int")> Public Property IsPrimaryKey As Integer
+    <Column(Name:="is_primary_key", DbType:="int")> Public Property IsPrimaryKey As Integer
 End Class
