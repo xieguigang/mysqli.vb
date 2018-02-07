38a2546ef0f94b1414a1924b099bcb579a148f57
xieguigang <xie.guigang@gcmodeller.org>
2018-02-07 13:22:12 +0800

    move directory

--- LibMySQL/Mysqli/Uri/ConnectionUri.vb
+++ LibMySQL/Mysqli/Uri/ConnectionUri.vb
@@ -1,31 +1,32 @@
 ﻿#Region "Microsoft.VisualBasic::693d17cbf1fb7a2c7872d13a6800e679, ..\mysqli\LibMySQL\MYSQL.Client\ConnectionUri.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports System.Text.RegularExpressions
 Imports System.Xml.Serialization
 
@@ -198,6 +199,8 @@ Public Class ConnectionUri
     ''' </summary>
     ''' <returns></returns>
     ''' <remarks></remarks>
+    ''' 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetConnectionString() As String
         Return Me.ToString
     End Function
@@ -209,7 +212,8 @@ Public Class ConnectionUri
     Public Overrides Function ToString() As String
         Dim cnn$ = __basicllyConfig() & ";charset=utf8"
 
-        If TimeOut >= 0 Then  ' 假若在这里timeout大于零的话，会在链接字符串中设置超时选项
+        If TimeOut >= 0 Then
+            ' 假若在这里timeout大于零的话，会在链接字符串中设置超时选项
             ' 等于零是无限等待
             cnn &= $" default command timeout={TimeOut};"
         End If
@@ -292,8 +296,8 @@ Public Class ConnectionUri
         Dim fieldHash As Dictionary(Of String, String) = (From s As String
                                                           In Profiles
                                                           Select __getValue(s)).ToDictionary
-#If debug Then
-          For Each item In fieldHash
+#If DEBUG Then
+        For Each item In fieldHash
             Call Console.WriteLine(item.ToString)
         Next
 #End If
