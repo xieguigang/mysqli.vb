ede54e3ccf5fa36edeb63c61165be4bc20885104
xieguigang <xie.guigang@gcmodeller.org>
2018-04-11 13:19:54 +0800

    add profile name

--- mysqliHelper/mysqli.vb
+++ mysqliHelper/mysqli.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::3eb46299b41fa1fafd1b64dae235af3c, mysqliHelper\mysqli.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module mysqli
-    ' 
-    '     Function: CopyToModule, LoadConfig
-    ' 
-    '     Sub: init, init_cli, (+2 Overloads) RunConfig, TestMySQLi
-    ' 
-    ' /********************************************************************************/
+' Module mysqli
+' 
+'     Function: CopyToModule, LoadConfig
+' 
+'     Sub: init, init_cli, (+2 Overloads) RunConfig, TestMySQLi
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -45,6 +45,8 @@ Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 Imports System.Text
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.Default
 Imports Microsoft.VisualBasic.SecurityString
 Imports Microsoft.VisualBasic.Terminal
 Imports Oracle.LinuxCompatibility.MySQL.Uri
@@ -113,6 +115,9 @@ Imports mysqliEndPoint = Oracle.LinuxCompatibility.MySQL.MySqli
 
     Const UpdateWarning$ = "MySQLi Manager will update your connection with these information: "
     Const dataFile$ = "mysqli.dat"
+    Const NameKey$ = "mysqli.profile"
+
+    ReadOnly defaultProfile As DefaultValue(Of String) = dataFile
 
     Public Sub RunConfig()
         Dim update As Action(Of ConnectionUri) =
@@ -121,8 +126,10 @@ Imports mysqliEndPoint = Oracle.LinuxCompatibility.MySQL.MySqli
                 Dim encrypt As New SHA256(key, 12345678)
                 Dim encrypted = mysqli.GenerateUri(AddressOf encrypt.EncryptData)
 
-                encrypted = encrypted & "|" & key
-                encrypted.SaveTo($"{App.LocalData}/{dataFile}", Encoding.ASCII)
+                With $"{App.LocalData}/{App.GetVariable(NameKey) Or defaultProfile}"
+                    encrypted = encrypted & "|" & key
+                    encrypted.SaveTo(.ByRef, Encoding.ASCII)
+                End With
             End Sub
 
         Call update.RunConfig
@@ -164,19 +171,25 @@ Imports mysqliEndPoint = Oracle.LinuxCompatibility.MySQL.MySqli
     End Sub
 
     ''' <summary>
-    ''' Load mysqli connection info from default config file.
+    ''' Load mysqli connection info from default config file. Profile name config from commandline:
+    ''' 
+    ''' ```bash
+    ''' /@set mysqli.profile='profileName'
+    ''' ```
     ''' </summary>
     ''' <returns></returns>
     Public Function LoadConfig() As ConnectionUri
-        Dim encrypted$ = ($"{App.LocalData}/{dataFile}").ReadAllText
+        Dim encrypted$ = ($"{App.LocalData}/{App.GetVariable(NameKey) Or defaultProfile}").ReadAllText
         Dim key = encrypted.Split("|"c).Last
-        encrypted = encrypted.Replace("|" & key, "")
         Dim descrypt As New SHA256(key, 12345678)
-        Dim uri As ConnectionUri = ConnectionUri.CreateObject(encrypted, AddressOf descrypt.DecryptString)
+        Dim uri As ConnectionUri = ConnectionUri.CreateObject(
+            url:=encrypted.Replace("|" & key, ""),
+            passwordDecryption:=AddressOf descrypt.DecryptString
+        )
         Return uri
     End Function
 
-    Public Function CopyToModule(exe$) As Boolean
+    Public Function CopyToModule(exe$, Optional dataFile$ = mysqli.dataFile) As Boolean
         Dim configuration$ = $"{App.LocalData}/{dataFile}".ReadAllText
         Dim target$ = App.GetAppLocalData(exe)
 
