14f525841d87f2701b3d94224847e781dee8d91d
xieguigang <xie.guigang@gcmodeller.org>
2018-09-12 19:50:52 +0800

    schema describe model

---
+++ LibMySQL/Reflection/SchemaDescribe.vb
@@ -0,0 +1,16 @@
+﻿Namespace Reflection
+
+    ''' <summary>
+    ''' ``DESCRIBE `db`.`tableName```
+    ''' </summary>
+    Public Class SchemaDescribe
+
+        Public Property Field As String
+        Public Property Type As String
+        Public Property Null As String
+        Public Property Key As String
+        Public Property [Default] As String
+        Public Property Extra As String
+
+    End Class
+End Namespace
