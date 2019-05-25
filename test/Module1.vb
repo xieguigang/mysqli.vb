c6bf80d0803c23ca28b7b40e94e4e8035cbafd2c
xieguigang <xie.guigang@gcmodeller.org>
2019-05-25 21:49:38 +0800

    get all local variable ??

--- test/Module1.vb
+++ test/Module1.vb
@@ -5,9 +5,10 @@ Module Module1
     Sub Main()
 
         Dim table As New Linq(Of mysql.visitor_stat)
+        Dim input As String = "192.168.%"
         Dim sql = BuildSQL(Function() From row As mysql.visitor_stat
                                       In table
-                                      Where row.app = 1 AndAlso row.ip Like "192.168.%"
+                                      Where row.app = 1 AndAlso row.ip Like input
                                       Select row.ip, row.success, row.time
                                       Order By ip Descending
                                       Distinct)
