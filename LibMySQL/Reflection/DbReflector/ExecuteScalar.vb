9cdf37b35648857d8baf797dcaa4a04e209afe19
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-09-27 10:48:51 +0800

    fix of namespace imports

--- LibMySQL/Reflection/DbReflector/ExecuteScalar.vb
+++ LibMySQL/Reflection/DbReflector/ExecuteScalar.vb
@@ -43,6 +43,7 @@
 
 Imports System.Reflection
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
 Imports Microsoft.VisualBasic.Language
