c6ecf3a7fa8825c71aa393e8d5d2a1616e99873c
xieguigang <xie.guigang@gcmodeller.org>
2021-05-05 16:03:10 +0800

    move to data schema module

--- DataSchema/My Project/Settings.Designer.vb
+++ DataSchema/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.Oracle.LinuxCompatibility.LibMySQL.My.MySettings
+        Friend ReadOnly Property Settings() As Global.Oracle.LinuxCompatibility.MySQL.My.MySettings
             Get
-                Return Global.Oracle.LinuxCompatibility.LibMySQL.My.MySettings.Default
+                Return Global.Oracle.LinuxCompatibility.MySQL.My.MySettings.Default
             End Get
         End Property
     End Module
