0368c719358589c7d8915ee37334a381eef1c5fd
谢桂纲 <gg.xie@bionovogene.com>
2023-11-23 10:56:06 +0800

    source file renames

--- LibMySQL/MySqlBuilder/FieldAssert.vb
+++ LibMySQL/MySqlBuilder/FieldAssert.vb
@@ -1,5 +1,4 @@
-﻿Imports System.Runtime.CompilerServices
-Imports Oracle.LinuxCompatibility.MySQL.Scripting
+﻿Imports Oracle.LinuxCompatibility.MySQL.Scripting
 
 Namespace MySqlBuilder
 
@@ -79,42 +78,5 @@ Namespace MySqlBuilder
                 Return $"'{val.MySqlEscaping}'"
             End If
         End Function
-
     End Class
-
-    Public Module ExpressionSyntax
-
-        ''' <summary>
-        ''' Create a new field reference
-        ''' </summary>
-        ''' <param name="name"></param>
-        ''' <returns></returns>
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        <DebuggerStepThrough>
-        Public Function field(name As String) As FieldAssert
-            Return New FieldAssert With {.name = $"`{name}`"}
-        End Function
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        <DebuggerStepThrough>
-        Public Function f(name As String) As FieldAssert
-            Return New FieldAssert With {.name = $"`{name}`"}
-        End Function
-
-        <Extension>
-        Public Function between(f As FieldAssert, min As String, max As String) As FieldAssert
-            f.op = NameOf(between)
-            f.val = FieldAssert.value(min)
-            f.val2 = FieldAssert.value(max)
-
-            Return f
-        End Function
-
-        Public Function [in](f As FieldAssert, vals As String()) As FieldAssert
-            f.op = NameOf([in])
-            f.val = vals.Select(Function(v) FieldAssert.value(v)).JoinBy(", ")
-            f.val = $"({f.val})"
-            Return f
-        End Function
-    End Module
 End Namespace
