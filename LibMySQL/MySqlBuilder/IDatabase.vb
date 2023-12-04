c4e7b7416097a56ba9315fd8b368b4ab8fcd4372
guigang xie <xie.guigang@gcmodeller.org>
2023-12-04 10:55:09 +0800

    create a new database connection

---
+++ LibMySQL/MySqlBuilder/IDatabase.vb
@@ -0,0 +1,11 @@
+﻿Imports Oracle.LinuxCompatibility.MySQL.Uri
+
+Public MustInherit Class IDatabase
+
+    Protected mysqli As ConnectionUri
+
+    Public Sub New(mysqli As ConnectionUri)
+        Me.mysqli = mysqli
+    End Sub
+
+End Class
