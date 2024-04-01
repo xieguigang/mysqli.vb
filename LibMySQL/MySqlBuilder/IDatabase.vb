7e1e7b88204156502591f9fb0c826258e08a360a
谢桂纲 <gg.xie@bionovogene.com>
2024-04-01 10:07:19 +0800

    add xml dev notes

--- LibMySQL/MySqlBuilder/IDatabase.vb
+++ LibMySQL/MySqlBuilder/IDatabase.vb
@@ -2,14 +2,25 @@
 Imports Oracle.LinuxCompatibility.MySQL.Reflection.DbAttributes
 Imports Oracle.LinuxCompatibility.MySQL.Uri
 
+''' <summary>
+''' an abstract database wrapper for multiple database table models in clr types
+''' </summary>
 Public MustInherit Class IDatabase
 
+    ''' <summary>
+    ''' the wrapper for the mysql query functions
+    ''' </summary>
     Protected mysqli As MySqli
 
     Public Sub New(mysqli As ConnectionUri)
         Me.mysqli = mysqli
     End Sub
 
+    ''' <summary>
+    ''' create a new data table model for create mysql query
+    ''' </summary>
+    ''' <typeparam name="T"></typeparam>
+    ''' <returns></returns>
     Protected Function model(Of T As MySQLTable)() As Model
         Return New Model(TableName.GetTableName(Of T), mysqli)
     End Function
