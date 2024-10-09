70173b510d728059ea8fd169d5375489973f3870
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-09 20:54:38 +0800

    make improvements of th counter tool

--- Reflector/CLI/PerformanceCounter.vb
+++ Reflector/CLI/PerformanceCounter.vb
@@ -9,13 +9,14 @@ Imports Oracle.LinuxCompatibility.MySQL.Uri
 Partial Module CLI
 
     <ExportAPI("/performance_counter")>
-    <Usage("/performance_counter -u <mysql_user_name> -p <password> [--host <mysql_host, default=localhost> --port <mysql_port, default=3306> --http <monitor_output, default=86>]")>
+    <Usage("/performance_counter -u <mysql_user_name> -p <password> [--host <mysql_host, default=localhost> --port <mysql_port, default=3306> --http <monitor_output, default=86> --save_size <max_pool_size, default=1000>]")>
     Public Function PerformanceCounter(args As CommandLine) As Integer
         Dim u As String = args("-u")
         Dim p As String = args("-p")
         Dim host As String = args("--host") Or "localhost"
         Dim port As Integer = args("--port") Or 3306
         Dim http As Integer = args("--http") Or 86
+        Dim save_size As Integer = args("--save_size") Or 1000
         Dim uri As New ConnectionUri() With {
             .Database = "performance_schema",
             .IPAddress = host,
@@ -23,7 +24,7 @@ Partial Module CLI
             .Port = port,
             .User = u
         }
-        Dim monitor As New Logger(uri, echo:=False)
+        Dim monitor As New Logger(uri, echo:=False, queue_size:=save_size)
         Dim service = New HttpDriver() _
             .HttpMethod("get", Sub(req, response)
                                    Dim url As URL = req.URL
@@ -36,7 +37,7 @@ Partial Module CLI
 
         Call New Thread(Sub() monitor.Run()).Start()
         Call Thread.Sleep(1000)
-        Call VBDebugger.EchoLine($"use api 'localhost:{http}/get/' for get mysql server performance counter data.")
+        Call VBDebugger.EchoLine($"use api 'http://localhost:{http}/get/' for get mysql server performance counter data.")
         Call service.Run()
 
         Return 0
