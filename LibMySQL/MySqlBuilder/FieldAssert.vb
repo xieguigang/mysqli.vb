969c4d278be1fdc2acca94660f4d1dfe3ad3699a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-31 16:24:33 +0800

    create bnackground model of pathway related disease

--- LibMySQL/MySqlBuilder/FieldAssert.vb
+++ LibMySQL/MySqlBuilder/FieldAssert.vb
@@ -1,60 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::7dd86cecdf16fa95a3ebc9ed779a2dac, src\mysqli\LibMySQL\MySqlBuilder\FieldAssert.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 269
-    '    Code Lines: 207
-    ' Comment Lines: 18
-    '   Blank Lines: 44
-    '     File Size: 9.31 KB
+' Summaries:
 
 
-    '     Class FieldAssert
-    ' 
-    '         Properties: name, op, unary_not, val, val2
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: against, ParseFieldName, ToString, value
-    '         Operators: (+2 Overloads) <, (+8 Overloads) <>, (+8 Overloads) =, (+2 Overloads) >, (+2 Overloads) Like
-    '                    (+2 Overloads) Not
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 269
+'    Code Lines: 207
+' Comment Lines: 18
+'   Blank Lines: 44
+'     File Size: 9.31 KB
+
+
+'     Class FieldAssert
+' 
+'         Properties: name, op, unary_not, val, val2
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: against, ParseFieldName, ToString, value
+'         Operators: (+2 Overloads) <, (+8 Overloads) <>, (+8 Overloads) =, (+2 Overloads) >, (+2 Overloads) Like
+'                    (+2 Overloads) Not
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Text.Parser
 Imports Oracle.LinuxCompatibility.MySQL.Scripting
 
@@ -145,6 +146,22 @@ Namespace MySqlBuilder
             Else
                 Return $"`{name}`"
             End If
+        End Function
+
+        ''' <summary>
+        ''' call on the function, current value is the function its first parameter
+        ''' </summary>
+        ''' <param name="func"></param>
+        ''' <param name="args"></param>
+        ''' <returns></returns>
+        Public Function call_on(func As String, ParamArray args As String()) As FieldAssert
+            Dim pars = {ToString()}.JoinIterates(args).ToArray
+            Dim calls As String = $"{func}({pars.JoinBy(", ")})"
+
+            Return New FieldAssert() With {
+                .op = "func",
+                .val = calls
+            }
         End Function
 
         ''' <summary>
