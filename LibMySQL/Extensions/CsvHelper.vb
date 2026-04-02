550b98ddb7fd8bf71c87cf5d89c5a0da9126c956
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-02 14:01:10 +0800

    split source file into multiple parts

---
+++ LibMySQL/Extensions/CsvHelper.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.Default
+Imports Oracle.LinuxCompatibility.MySQL.Reflection.DbAttributes
+Imports Oracle.LinuxCompatibility.MySQL.Reflection.Schema
+Imports Oracle.LinuxCompatibility.MySQL.Reflection.SQL
+Imports ASCII = Microsoft.VisualBasic.Text.ASCII
+
+Public Module CsvHelper
+
+    ''' <summary>
+    ''' 将csv导入数据库之中的帮助工具，利用这个工具解析csv文件的头部标题行，生成``Create Table``脚本
+    ''' </summary>
+    ''' <param name="path"></param>
+    ''' <returns></returns>
+    Public Function CsvImportsHelper(path As String) As String
+        Dim header$() = path _
+            .ReadFirstLine _
+            .Split(","c) _
+            .Select(Function(s) s.Trim(ASCII.Quot)) _
+            .ToArray
+        Dim fields As Dictionary(Of String, Field) = header _
+            .ToDictionary(Function(name) name,
+                          Function(name)
+                              Return simpleField(name)
+                          End Function)
+        Dim schema As New Table(fields) With {
+            .TableName = path.BaseName,
+            .Comment = $"Auto-generated table schema from csv file: {path}",
+            .PrimaryFields = New List(Of String) From {header.First},
+            .Database = path.ParentDirName
+        }
+        Dim SQL$ = CreateTableSQL.FromSchema(schema)
+        Return SQL
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    <Extension>
+    Private Function simpleField(name As String) As Field
+        Return New Field() With {
+            .FieldName = name,
+            .DataType = New DataType(MySqlDbType.Text)
+        }
+    End Function
+End Module
--- LibMySQL/Extensions/DataDumps.vb
+++ LibMySQL/Extensions/DataDumps.vb
@@ -60,7 +60,6 @@ Imports Microsoft.VisualBasic.Language.Default
 Imports Microsoft.VisualBasic.Text
 Imports Oracle.LinuxCompatibility.MySQL.Reflection.DbAttributes
 Imports Oracle.LinuxCompatibility.MySQL.Reflection.Schema
-Imports Oracle.LinuxCompatibility.MySQL.Reflection.SQL
 Imports ASCII = Microsoft.VisualBasic.Text.ASCII
 
 Public Module DataDumps
@@ -302,40 +301,5 @@ Public Module DataDumps
         End If
 
         Return sql.SaveTo(path, encoding.CodePage)
-    End Function
-
-    ''' <summary>
-    ''' 将csv导入数据库之中的帮助工具，利用这个工具解析csv文件的头部标题行，生成``Create Table``脚本
-    ''' </summary>
-    ''' <param name="path"></param>
-    ''' <returns></returns>
-    Public Function CsvImportsHelper(path As String) As String
-        Dim header$() = path _
-            .ReadFirstLine _
-            .Split(","c) _
-            .Select(Function(s) s.Trim(ASCII.Quot)) _
-            .ToArray
-        Dim fields As Dictionary(Of String, Field) = header _
-            .ToDictionary(Function(name) name,
-                          Function(name)
-                              Return simpleField(name)
-                          End Function)
-        Dim schema As New Table(fields) With {
-            .TableName = path.BaseName,
-            .Comment = $"Auto-generated table schema from csv file: {path}",
-            .PrimaryFields = New List(Of String) From {header.First},
-            .Database = path.ParentDirName
-        }
-        Dim SQL$ = CreateTableSQL.FromSchema(schema)
-        Return SQL
-    End Function
-
-    <MethodImpl(MethodImplOptions.AggressiveInlining)>
-    <Extension>
-    Private Function simpleField(name As String) As Field
-        Return New Field() With {
-            .FieldName = name,
-            .DataType = New DataType(MySqlDbType.Text)
-        }
     End Function
 End Module
