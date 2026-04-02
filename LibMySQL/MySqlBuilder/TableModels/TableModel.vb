fc3e609b33a384085851c2fc57f5ea046dc967aa
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-02 14:18:57 +0800

    split sourcerr file into multiple parts

---
+++ LibMySQL/MySqlBuilder/TableModels/TableModel.vb
@@ -0,0 +1,22 @@
+﻿
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Language
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+Imports Oracle.LinuxCompatibility.MySQL.Reflection.DbAttributes
+
+Public Class TableModel(Of T As {New, MySQLTable}) : Inherits Model
+
+    Sub New(mysqli As MySqli)
+        Call MyBase.New(TableName.GetTableName(Of T), mysqli)
+    End Sub
+
+    Public Function find_object(ParamArray where As FieldAssert()) As T
+        Return Me.where(where).find(Of T)
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function async() As AsyncModelTable
+        Return New AsyncModelTable(Me)
+    End Function
+
+End Class
